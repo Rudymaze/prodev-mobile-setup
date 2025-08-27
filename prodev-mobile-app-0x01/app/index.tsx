41b47e52c18b427b2b6810ae142f18c683f7f0d4
RUDYMAZE <njockrudiger4@gmail.com>
2025-08-27 21:36:57 +0300

    first commit

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -4,13 +4,13 @@ export default function Index() {
   return (
     <View style={styles.container}>
       <Text>Entry Screen - Awesome</Text>
-      <Text style={styles.largeText}>
+      <Text style={styles.additionalText1}>
         Typescript is great if you practice more
       </Text>
-      <Text style={styles.mediumText}>
+      <Text style={styles.additionalText2}>
         React Native provides you a single codebase for cross platforms
       </Text>
-      <Text style={styles.smallText}>ALX is awesome</Text>
+      <Text style={styles.additionalText3}>ALX is awesome</Text>
     </View>
   );
 }
@@ -38,5 +38,29 @@ const styles = StyleSheet.create({
     color: "#2196f3",
     fontWeight: "400",
     textAlign: "center",
+  },
+  additionalText1: {
+    fontSize: 25,
+    color: "#4caf50",
+    marginTop: 15,
+    fontWeight: "600",
+    textAlign: "left",
+    fontStyle: "italic",
+  },
+  additionalText2: {
+    fontSize: 18,
+    color: "#ff9800",
+    marginTop: 10,
+    fontWeight: "bold",
+    textAlign: "center",
+    textDecorationLine: "underline",
+  },
+  additionalText3: {
+    fontSize: 16,
+    color: "#795548",
+    marginTop: 8,
+    fontWeight: "400",
+    textAlign: "right",
+    fontVariant: ["small-caps"],
   },
 });
